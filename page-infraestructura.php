94275bb5f6f2fc46cd19b3979b19441c22523ce4
Byron-Guevara <byronguevara@akevia.mx>
2020-01-08 12:06:33 -0600

    Servicios, Infraestructura responsive, con banner y contacto responsive

--- page-infraestructura.php
+++ page-infraestructura.php
@@ -6,8 +6,10 @@
 ?>
 <?php get_header();?>
 
-<div class="head">
-
+<div class="banner" style="background-image: url(<?php echo get_the_post_thumbnail_url(); ?>)">
+    <div class="banner-title">
+        <?php the_title() ?>
+    </div>
 </div>
 
 
--- page-nosotros.php
+++ page-nosotros.php
@@ -1,3 +1,10 @@
+<?php
+/**
+ * Template Name: Nosotros
+ *
+ */
+?>
+
 <?php get_header() ?>
 <div class="banner" style="background-image: url(<?php echo get_the_post_thumbnail_url(); ?>)">
     <div class="banner-title">
